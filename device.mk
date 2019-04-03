705fbbba565b616407fc69ae0def76c741f58427
Murat Kozan <muratkozan350@gmail.com>
2019-04-03 16:21:12 +0300

    kuntao: Add advanced controls

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Advanced controls
+PRODUCT_PACKAGES += \
+    AdvancedControls
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
